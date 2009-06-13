991d62e80eb49b58e1900e7fa212f64d485469d8
angela <angela.chung1@gmail.com>
2009-06-13 15:55:47 -0700

    added votes to topic

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,14 @@
+class VotesController < ApplicationController
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @vote = @topic.votes.create!
+    flash[:notice] = "You voted for #{@vote.topic.title}"
+    redirect_to(topics_path)
+   end
+   
+  def destroy
+    @vote = Topic.find(params[:topic_id]).votes.last
+    @vote.destroy
+    redirect_to(topics_url)
+  end
+end
